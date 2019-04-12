85fd97bcad57e5f71c96a399d01261f087625238
xtremerox <ramatheracer@gmail.com>
2019-04-13 03:41:07 +0530

    create ass api

--- api/v0/assesments.js
+++ api/v0/assesments.js
@@ -9,9 +9,15 @@ router.post("/", (req,res)=>{
     query = "CREATE TABLE assesment_"+data.assesment_name+" (id int not null auto_increment, question varchar(100) not null, opt1 varhcar(50) not null, opt2 varchar(50) not null, opt3 varchar(50)not null, opt4 varchar(50) not null, correct_opt int not null, primary key(id))";
     db.query(query, [], (err, results, fields)=>{
         if(err){
-            res.send({code:"error", message:err.message});
+            res.send({code: "error", message:err.message});
             return;
         }
+        db.query("INSERT INTO assesments (group_title, assesment_name, enable) VALUES (?,?,?)", [data.group_title, data.assesment_name, data.enable],(err, results, fields)=>{
+            if(err){
+                res.send({code: "error", message:err.message});
+                return;
+            }
+        })
         res.send({code: "success"})
     }) 
 });
