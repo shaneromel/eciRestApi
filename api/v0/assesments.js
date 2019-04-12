7dbd8eb95431951a46059fffc837edc8e634bd2d
xtremerox <ramatheracer@gmail.com>
2019-04-13 04:04:08 +0530

    completed get assesment

--- api/v0/assesments.js
+++ api/v0/assesments.js
@@ -3,10 +3,10 @@ var router=express.Router();
 var db=require("../../utils/db");
 
 //group_title assesment_name enable
-router.post("/", (req,res)=>{
+router.post("/create", (req,res)=>{
     const data = req.body;
     let query;
-    query = "CREATE TABLE assesment_"+data.assesment_name+" (id int not null auto_increment, question varchar(100) not null, opt1 varchar(50) not null, opt2 varchar(50) not null, opt3 varchar(50)not null, opt4 varchar(50) not null, correct_opt int not null, primary key(id))";
+    query = "CREATE TABLE assesment_"+data.assesment_name+" (id int not null auto_increment, question varchar(100) not null, opt1 varhcar(50) not null, opt2 varchar(50) not null, opt3 varchar(50)not null, opt4 varchar(50) not null, correct_opt int not null, primary key(id))";
     db.query(query, [], (err, results, fields)=>{
         if(err){
             res.send({code: "error", message:err.message});
@@ -17,11 +17,21 @@ router.post("/", (req,res)=>{
                 res.send({code: "error", message:err.message});
                 return;
             }
-
-            res.send({code:"success"})
-
         })
+        res.send({code: "success"})
     }) 
+});
+
+router.get("/", (req, res)=>{
+    let query;
+    query = "SELECT * FROM assesments";
+    db.query(query, [], (err, results, fields)=>{
+        if(err){
+            res.send({code: "error", message: err.message});
+            return;
+        }
+        res.send({code:"success", data:results})
+    })
 });
 
 module.exports = router;
