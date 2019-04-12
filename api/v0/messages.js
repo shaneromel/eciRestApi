836cce7215d51b9cd0b433106e67a31e9d832b53
xtremerox <ramatheracer@gmail.com>
2019-04-13 00:15:41 +0530

    messages get

--- api/v0/messages.js
+++ api/v0/messages.js
@@ -18,4 +18,15 @@ router.post("/", (req, res)=>{
 
 });
 
+router.get("/:group_title", (req,res)=>{
+    const group_title = req.params.group_title;
+    db.query("SELECT * FROM messages WHERE group_title = ? ORDER BY timestamp DESC;", [group_title], (err, results, fields)=>{
+        if(err){
+            res.send({code: "error", message:err.message});
+            return;
+        }
+        res.send({code:"success", data: results})
+    })
+});
+
 module.exports=router;
