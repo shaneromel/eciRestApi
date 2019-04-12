f0650e87e85c6f5d81d81d789f01e5e347b1f010
shaneromel <shaneromelkujur@gmail.com>
2019-04-13 03:58:39 +0530

    assessments added

--- api/v0/assesments.js
+++ api/v0/assesments.js
@@ -6,7 +6,7 @@ var db=require("../../utils/db");
 router.post("/", (req,res)=>{
     const data = req.body;
     let query;
-    query = "CREATE TABLE assesment_"+data.assesment_name+" (id int not null auto_increment, question varchar(100) not null, opt1 varhcar(50) not null, opt2 varchar(50) not null, opt3 varchar(50)not null, opt4 varchar(50) not null, correct_opt int not null, primary key(id))";
+    query = "CREATE TABLE assesment_"+data.assesment_name+" (id int not null auto_increment, question varchar(100) not null, opt1 varchar(50) not null, opt2 varchar(50) not null, opt3 varchar(50)not null, opt4 varchar(50) not null, correct_opt int not null, primary key(id))";
     db.query(query, [], (err, results, fields)=>{
         if(err){
             res.send({code: "error", message:err.message});
@@ -17,8 +17,10 @@ router.post("/", (req,res)=>{
                 res.send({code: "error", message:err.message});
                 return;
             }
+
+            res.send({code:"success"})
+
         })
-        res.send({code: "success"})
     }) 
 });
 
