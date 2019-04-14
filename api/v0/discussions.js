5c9b495531ef7138d04a49b7ce6b7417ed073dc0
xtremerox <ramatheracer@gmail.com>
2019-04-14 15:20:43 +0530

    done api

--- api/v0/discussions.js
+++ api/v0/discussions.js
@@ -43,14 +43,13 @@ router.post("/", (req,res)=>{
     const data = req.body;
     const uid = data.uid;
     const topic = data.topic;
-    let query;
     db.query("SELECT name FROM members WHERE uid = ? ", [uid], (err, results, fields)=>{
         if(err){
             res.send({code:"error", message:err.message});
             return;
         }
         const name = results[0].name;
-        db.query("INSERT INTO discussion (opened_by, topic, name_opened_by) VALUES (?,?,?)",[data.uid, data.topic, name],(err,results,fields)=>{
+        db.query("INSERT INTO threads (opened_by, topic, name_opened_by) VALUES (?,?,?)",[data.uid, data.topic, name],(err,results,fields)=>{
             if(err){
                 res.send({code:"error", message:err.message});
                 return;
@@ -60,6 +59,89 @@ router.post("/", (req,res)=>{
     })
 });
 
+//uid thread_id
+router.post("/close", (req, res)=>{
+    const data = req.body;
+    const uid = data.uid;
+    const thread_id = data.thread_id;
+    let query;
+    db.query("SELECT opened_by FROM threads WHERE id = ?",[thread_id], (err, results, fields)=>{
+        if(err){
+            res.send({code:"error", message:err.message});
+            return;
+        }
+        if(results[0].opened_by != uid && uid != "admin"){
+            res.send({code:"error", message:"You didn't open the thread"})
+            return;
+        }else{
+            db.query("UPDATE threads SET isopened = '0' WHERE id = ?",[thread_id], (err, results, fields)=>{
+                if(err){
+                    res.send({code:"error", message:err.message})
+                    return;
+                }
+                res.send({code:"success"});
+            })
+        }
+    })
+});
 
+//thread_id uid meesage
+router.post("/message/:thread_id", (req,res)=>{
+    const data = req.body;
+    const uid = data.uid;
+    const contents = data.content;
+    const thread_id = req.params.thread_id;
+    isopened(thread_id).then(()=>{
+        getnameofuid(uid).then((name)=>{
+            db.query("INSERT INTO discussion (thread_id, content, uid, name_by, timestamp) VALUES (?,?,?,?,?)", [thread_id, contents, uid, name, Date.now()], (err, results, fields)=>{
+                if(err){
+                    res.send({code:"error", message:err.message});
+                    return;
+                }
+                res.send({code:"success"});
+            })
+        }).catch(message=>{
+            res.send({code:"error", message:message});
+            return;
+        });
+    }).catch(a=>{
+            res.send({code:"error", message:a});     
+            return;
+    });
+});
+
+var isopened = (thread_id)=>{
+    return new Promise((resolve, reject)=>{
+        db.query("SELECT isopened FROM threads WHERE id = ?", [thread_id], (err, results, fields)=>{
+            if(err){
+                reject(err.message);
+            }
+            if(results.length>0){
+                if(results[0].isopened === 1){
+                    resolve(true);
+                }else{
+                    reject("Thread is closed");
+                }
+            }else{
+                reject("Thread doesn't exist");
+            }
+        })
+    })
+}
+
+var getnameofuid = (uid)=>{
+   return new Promise((resolve, reject)=>{
+    db.query("SELECT name FROM members WHERE uid = ?",[uid],(err, results, fields)=>{
+        if(err){
+            reject("Couldn't get name");
+        }
+            if(results.length>0){
+                resolve(results[0].name);
+            }else{
+                reject("User doesn't exist");
+            }
+        });
+   })
+}
 
 module.exports = router;
