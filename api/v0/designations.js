4b8e40cb92b04e96aabf6cc903e2064936445f14
xtremerox <ramatheracer@gmail.com>
2019-04-17 13:25:56 +0530

    designations

--- api/v0/designations.js
+++ api/v0/designations.js
@@ -12,6 +12,36 @@ router.get("/", (req, res)=>{
         res.send({code:"success", data:results});
 
     })
+});
+
+router.post("/", (req, res)=>{
+    const data=req.body;
+
+    db.query("INSERT INTO designations (title) VALUES (?)", [data.title], (err, results, fields)=>{
+        if(err){
+            res.send({code:"error", message:err.message});
+            return;
+        }
+
+        res.send({code:"success"});
+
+    })
+
+})
+
+router.delete("/:id", (req, res)=>{
+    const id=req.params.id;
+
+    db.query("DELETE FROM designations WHERE id = ?", [id], (err, results, fields)=>{
+        if(err){
+            res.send({code:"error", message:err.message});
+            return;
+        }
+
+        res.send({code:"success"});
+
+    })
+
 })
 
 module.exports=router;
