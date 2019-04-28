cb8596e6e7e61f3c656c3f6d1f65d5c53f91b715
xtremerox <ramatheracer@gmail.com>
2019-04-28 11:52:12 +0530

    envents

--- api/v0/events.js
+++ api/v0/events.js
@@ -197,7 +197,7 @@ function dateToTimestamp(date, time){
 
     const convertedTime=timeConverter(formattedTime);
 
-    const formattedDate=new Date(dateArray[2], dateArray[0], dateArray[1], convertedTime.hour, convertedTime.min, 0);
+    const formattedDate=new Date(dateArray[2], parseInt(dateArray[0])-1, dateArray[1], convertedTime.hour, convertedTime.min, 0);
 
     return formattedDate.getTime();
 
